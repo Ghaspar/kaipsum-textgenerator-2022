15cf4b7f3dd6e4b47c284edcdd410ff2c5831c13
Lucas <lucas.ghaspar@gmail.com>
2022-11-04 20:49:03 -0300

    feat: make content component

---
+++ components/Content/index.tsx
@@ -0,0 +1,21 @@
+import styles from './style.module.scss'
+
+type DataProps = {
+  children: any;
+};
+
+
+export default function Header() {
+
+  return (
+   <>
+        <main>
+            <div className="container">
+                <div className="row">
+                    
+                </div>
+            </div>
+        </main>
+   </>
+  )
+}
